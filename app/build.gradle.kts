17bb920c13770cd03a68693b80981b2d8ac6c6e1
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2026-03-20 16:03:40 -0400

    chore: Sync retrofit, gson, coroutines and viewmodel dependences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson)
+    implementation(libs.kotlinx.coroutines.core)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
